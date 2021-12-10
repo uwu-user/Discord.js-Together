3f636347a8f0a371377d807bd91d975375dfbc50
Unknown_ <71901584+uwu-user@users.noreply.github.com>
2021-12-11 00:25:33 +0300

    Updated
    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Discord, MessageEmbed, Intents, Client } = require("discord.js");
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-});
+const client = new Client({  
+  intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES 
+       ]});
 
 client.login("You bot TOKEN");
 
@@ -10,17 +10,17 @@ client.on("messageCreate", message => {
   if (message.content.startsWith("djs!Game")) {
     try {
       let args = message.content.split(" ").slice(1);
-      let c = message.guild.channels.cache.find(x => x.name == args[0]);
+      const channel = message.member.voice.channel;
       let Embed = new MessageEmbed()
         .setTitle("command: Discord Together")
-        .addField(`Usage:`, `djs!Game (voice channel name) (Game ID)`, inline: true)
-        .addField(`Examples: `, `djs!Game Gamesvoice 755600276941176913`, inline: true);
-      if (!c || c.type !== "GUILD_VOICE" || isNaN(args[1]))
+        .addField(`Usage:`, `djs!Game (voice channel name) (Game ID)`)
+        .addField(`Examples: `, `djs!Game Gamesvoice 755600276941176913`);
+      if (!channel)
         return message.channel.send({ embeds: [Embed] });
       c.createInvite({
         maxUses: 0,
         maxAge: 86400,
-        targetApplication: args[1],
+        targetApplication: args[1], // game ID
         targetType: 2,
         temporary: false
       }).then(content => message.channel.send({ content: content.url }));
